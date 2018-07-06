d551698b13fa53b38d106bb564f849c58e16fda9
Dimitris Katsios <d.katsios@gmail.com>
2018-07-06 12:17:16 +0900

    Update evaluation.py

--- evaluation.py
+++ evaluation.py
@@ -1,8 +1,8 @@
-#!/usr/bin/env python2
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
-from __future__ import unicode_literals
+# #!/usr/bin/env python2
+# from __future__ import absolute_import
+# from __future__ import division
+# from __future__ import print_function
+# from __future__ import unicode_literals
 import os, sys
 import numpy as np
 import json
